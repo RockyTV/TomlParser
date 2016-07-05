917da260fa0efa9a0c505ec63e1bfc5cc70cd195
Alexandre Oliveira <rockytvbr@gmail.com>
2016-07-05 16:11:44 -0300

    Change testing project to UnitTests

---
+++ UnitTests/KeyValueTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+using NUnit.Framework;
+
+namespace TomlParser.UnitTests
+{
+	[TestFixture]
+	public class TomlKeyValueTest
+	{
+		private string fileName;
+		private TomlDocument doc = null;
+
+		[SetUp]
+		public void Init()
+		{
+			fileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "test.toml");
+			doc = TomlParser.FromFile(fileName);
+		}
+
+		[Test]
+		public void IsValidName()
+		{
+			Assert.Throws<InvalidKeyNameException>(delegate { new TomlKeyValue("!@#$%¨&*()key", null); });
+			Assert.Throws<InvalidKeyNameException>(delegate { new TomlKeyValue("[key]", null); });
+			Assert.DoesNotThrow(delegate { new TomlKeyValue("k_ey", null); });
+			Assert.DoesNotThrow(delegate { new TomlKeyValue("k-ey", null); });
+			Assert.DoesNotThrow(delegate { new TomlKeyValue("1234", null); });
+		}
+	}
+}
---
+++ UnitTests/ParserTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+using NUnit.Framework;
+
+namespace TomlParser.UnitTests
+{
+	[TestFixture]
+	public class TomlParserTest
+	{
+		private string fileName;
+		private TomlDocument doc = null;
+
+		[SetUp]
+		public void Init()
+		{
+			fileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "test.toml");
+			doc = TomlParser.FromFile(fileName);
+		}
+
+		[Test]
+		public void ParseDocument()
+		{
+			TomlDocument otherDoc = TomlParser.FromFile(fileName);
+			Assert.Inconclusive("There's no way to compare if both documents are equal yet.");
+		}
+	}
+}
